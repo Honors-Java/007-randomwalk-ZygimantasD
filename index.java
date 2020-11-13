344b9ff5db518780718970e9410e30cbe9eb06eb
Zygimantas Daunoras <daunoraszygimantas@gmail.com>
2020-11-13 17:02:26 +0000

    made a random walking simulator

--- index.java
+++ index.java
@@ -1,7 +1,5 @@
-//Put all your variables up here outside the setup or draw functions
-//What variables may you want?
-//  x, y, number of steps to take, and current steps taken
 
+// variables: x = xaxis, y = yaxis, n = limit of steps, steps = counter
 int n = 300;
 int steps = 0;
 int x = 200;
@@ -10,22 +8,53 @@ int y = 200;
 
 void setup() {
 	size(500, 500);
-  frameRate(10);
+  	frameRate(10);
 }
+
 
 void draw() {
 	stroke(1);
-  //starts random walking from 200,200
-  rect(x, y, 10, 10);
-  
-  //Here is where you should create the new rectangles to create the illusion of 'random walking'
-  // You will need to make sure you dont draw more rectangles than the limit of steps
-  //You'll need to use the Math.random() to decide how to create randomness in your drawing of rectangles.
-  // Keep your canvas and rectangle width and height as I've given you
+	fill(255);
+	rect(x, y, 10, 10);
+	steps++; // adds 1 after rect() is drawn
 
- 
-  
-    println("drew rect at: " + x + ", " + y);
+	if(steps == n) {
+		noLoop(); // stops draw()
+	}else {
+		int z = (int)(Math.random() * 4);
+
+		if(z == 0) {
+			// nested ifs supposedly contain the rect() within the canvas
+			if(x < 490) {
+				x+=10;
+			}else {
+				x-=10;
+			}
+		}else if(z == 1) {
+			if(y > 0) {
+				y-=10;
+			}else {
+				y+=10;
+			}
+		}else if(z == 2) {
+			if(x > 0) {
+				x-=10
+			}else {
+				x+=10;
+			}
+		}else {
+			if(y < 490) {
+				y+=10;
+			}else {
+				y-=10;
+			}
+		}
+
+		fill(0, 0, 255); 
+		rect(x, y, 10, 10); // it is drawn the original rect()
+	}
+
+	println("drew rect at: " + x + ", " + y);
     println("Steps taken: " + steps);
 }
 
